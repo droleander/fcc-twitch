f668893c186ec1829d7e33bf2f7d8579c3691807
Leandro <lileandermail+gh+@gmail.com>
2019-06-02 01:15:37 +0800

    Updated

--- js/twitch.js
+++ js/twitch.js
@@ -1,206 +1,101 @@
-/**
- * Pseudocode
- * 
- * - After loading the page...
- * 
- * - Check each username in users[] if it exists or not in Twitch.tv via usersAPI
- * - If username exists, push to usersFound[]
- * - If username does not exist, push to usersNotFound[]
- * 
- * - Check each username in usersFound[] if it is streaming or not via streamsAPI
- * - If username is streaming, push to usersStreaming[]
- * - If username is not streaming, push to usersNotStreaming[]
- * 
- * - For each username in usersStreaming[], get its logo, game, status via streamsAPI
- *   then push to usersOnline[] and usersAll[]
- *   
- * - For each username in usersNotStreaming[], get its logo, game via channelsAPI and
- *   set the status to 'Offline' then push to usersOffline[] and usersAll[]
- * 
- * - Display ALL / ONLINE / OFFLINE users
- * - Display ALL users found in usersAll[]
- * - Display ONLINE users found in usersOnline[]
- * - Display OFFLINE users found in usersOffline[]
- */
+// Pseudocode
+// - After loading the page...
+ 
+// Check each username in users[] if it exists or not in Twitch.tv via usersAPI
+// - If username exists, push to usersFound[]
+// - If username does not exist, push to usersNotFound[]
+
+// Check each username in usersFound[] if it is streaming or not via streamsAPI
+// - If username is streaming, push to usersStreaming[]
+// - If username is not streaming, push to usersNotStreaming[]
+
+// For each username in usersStreaming[], get its logo, game, status via streamsAPI
+// then push to usersOnline[] and usersAll[]
+
+// For each username in usersNotStreaming[], get its logo, game via channelsAPI and
+// set the status to 'Offline' then push to usersOffline[] and usersAll[]
+
+// Display ALL / ONLINE / OFFLINE users
+// Display ALL users found in usersAll[]
+// Display ONLINE users found in usersOnline[]
+// Display OFFLINE users found in usersOffline[]
 
 
-/* Variables */
+const BASE_API = 'https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api';
+const USERS_API = BASE_API + "/users/";
+const STREAMS_API = BASE_API + "/streams/";
+const CHANNELS_API = BASE_API + "/channels/";
 
-let baseAPI = 'https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api';
-let usersAPI = baseAPI + "/users/";
-let streamsAPI = baseAPI + "/streams/";
-let channelsAPI = baseAPI + "/channels/";
+let usersAll = [], 
+	usersFound = [], 
+	usersNotFound = [], 
+	usersStreaming = [],
+	usersNotStreaming = [], 
+	usersOnline = [], 
+	usersOffline = [], 
+	strLogo = '', 
+	strGame = '', 
+	strStatus = '', 
+	strHTML = '';
 
-let users = [
-  "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck",
-  "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"
-];
 
-let usersAll = [];
-let usersFound = [];
-let usersNotFound = [];
-let usersStreaming = [];
-let usersNotStreaming = [];
-let usersOnline = [];
-let usersOffline = [];
-
-let strLogo = '';
-let strGame = '';
-let strStatus = '';
-let strHTML = '';
-
-/* Functions */
-
-function getUsers() {
-	checkUsers();
-	checkStreamers();
-	getOnlineStreamerInfo();
-	getOfflineStreamerInfo();
-}
-
-function checkUsers() {
-	// - Check each username in users[] if it exists or not in Twitch.tv via usersAPI
-	// - If username exists, push to usersFound[]
-	// - If username does not exist, push to usersNotFound[]
+async function getTwitchUsers(strUser) {
+	let streamInfo = await fetch(STREAMS_API + strUser);
 	
-	// usersFound = [];
-	// usersNotFound = [];
-
-	users.forEach(async user => {
-		let response = await fetch(usersAPI + user);
-
-		if (response.ok) {
-			usersFound.push(user);
+	if (streamInfo.ok && streamInfo.status === 200) {
+		let streamData = await streamInfo.json();
+	
+		if (streamData.stream !== null) {
+			strLogo = streamData.stream.channel.logo;
+			strGame = streamData.stream.channel.game;
+			strStatus = streamData.stream.channel.status;
+		
+			usersOnline.push([strLogo, strUser, strGame, strStatus]);
+	
 		} else {
-			usersNotFound.push(user);
+			let channelsInfo = await fetch(CHANNELS_API + strUser);
+			let ChannelsData = await channelsInfo.json();
+			strLogo = ChannelsData.logo;
+			strGame = ChannelsData.game;
+			strStatus = 'offline';
+		
+			usersOffline.push([strLogo, strUser, strGame, strStatus]);
 		}
-	});
-
-	/* WORKING BUT VERY SLOW
-	for (var counter = 0; counter < users.length; counter++) {
-		$.ajax({
-			url: usersAPI + users[counter],
-			async: false,
-			cache: false,
-			success: function (checkedUser) {
-				if (checkedUser.error) {
-					usersNotFound.push(users[counter]);
-				} else {
-					usersFound.push(users[counter]);
-				}
-			},
-			error: function (checkedUser) {
-				usersNotFound.push(users[counter]);
-			}
-		});
-	}
-	*/
-}
-
-function checkStreamers() {
-	// - Check each username in usersFound[] if it is streaming or not via streamsAPI
-	// - If username is streaming, push to usersStreaming[]
-	// - If username is not streaming, push to usersNotStreaming[]
 	
-	// usersStreaming = [];
-	// usersNotStreaming = [];
-
-	// WORKING BUT VERY SLOW
-	for (var counter = 0; counter < usersFound.length; counter++) {
-		$.ajax({
-			url: streamsAPI + usersFound[counter],
-			async: false,
-			cache: false,
-			success: function (checkedStreamer) {
-				var strStreams = JSON.stringify(checkedStreamer.stream);
-				if (strStreams != "null") {
-					usersStreaming.push(usersFound[counter]);
-				} else {
-					usersNotStreaming.push(usersFound[counter]);
-				}
-			},
-			error: function (checkedStreamer) {
-				usersNotStreaming.push(usersFound[counter]);
-			}
-		});
-	}
-}
-
-function getOnlineStreamerInfo() {
-	// - For each username in usersStreaming[], get its logo, game, status via streamsAPI
-	//   then push to usersOnline[] and usersAll[]
-	
-	// usersOnline = [];
-	
-	// WORKING BUT VERY SLOW
-	for (var counter = 0; counter < usersStreaming.length; counter++) {
-		$.ajax({
-			url: streamsAPI + usersStreaming[counter],
-			async: false,
-			cache: false,
-			success: function (onlineStreamerData) {
-				strLogo = onlineStreamerData.stream.channel.logo;
-				strGame = onlineStreamerData.stream.channel.game;
-				strStatus = onlineStreamerData.stream.channel.status;
-				usersOnline.push([strLogo, usersStreaming[counter], strGame, strStatus]);
-				usersAll.push([strLogo, usersStreaming[counter], strGame, strStatus]);
-			}
-		});
-	}
-}
-
-function getOfflineStreamerInfo() {
-	// - For each username in usersNotStreaming[], get its logo, game via channelsAPI and
-	//   set the status to 'Offline' then push to usersOffline[] and usersAll[]
-	
-	// usersOffline = [];
-	
-	// WORKING BUT VERY SLOW
-	for (var counter = 0; counter < usersNotStreaming.length; counter++) {
-		$.ajax({
-			url: channelsAPI + usersNotStreaming[counter],
-			async: false,
-			cache: false,
-			success: function (offlineStreamerData) {
-				strLogo = offlineStreamerData.logo;
-				strGame = offlineStreamerData.game;
-				strStatus = 'offline';
-				usersOffline.push([strLogo, usersNotStreaming[counter], strGame, strStatus]);
-				usersAll.push([strLogo, usersNotStreaming[counter], strGame, strStatus]);
-			}
-		});
+		usersAll.push([strLogo, strUser, strGame, strStatus]);
 	}
 }
 
 function displayList(arr) {
-	// - Display ALL | ONLINE | OFFLINE users
-	// - Display ALL users found in usersAll[]
-	// - Display ONLINE users found in usersOnline[]
-	// - Display OFFLINE users found in usersOffline[]
-	
 	$(".section-content").empty();
 	for (var counter = 0; counter < arr.length; counter++) {
 		if (arr[counter][3] != "offline") {
-			strHTML = '<div class="row content user-online">';
+			strHTML = `<div class="row content user-online">`;
 		} else {
-			strHTML = '<div class="row content user-' + arr[counter][3] + '">';
+			strHTML = `<div class="row content user-${arr[counter][3]}">`;
 		}
-		strHTML += '<div class="col-xs-2">';
-		strHTML += '<img src="' + arr[counter][0] + '" alt="logo">'; // logo
-		strHTML += '</div>';
-		strHTML += '<div class="col-xs-10 content-detail">';
-		strHTML += '<p><a href="https://www.twitch.tv/' + arr[counter][1] + '" target="_blank">' + arr[counter][1] + '</a></p>'; // username
+		strHTML += `<div class="col-xs-2">
+			<img src="${arr[counter][0]}" alt="logo">
+			</div>
+			<div class="col-xs-10 content-detail">
+			<p><a href="https://www.twitch.tv/${arr[counter][1]}" target="_blank">${arr[counter][1]}</a></p>`;
 		if (arr[counter][3] != "offline") {
-			strHTML += '<p>' + arr[counter][2] + '</p>'; // game  
+			strHTML += `<p>${arr[counter][2]}</p>`; // game  
 		}
-		strHTML += '<p>' + arr[counter][3] + '</p>'; // status
-		strHTML += '</div>';
-		strHTML += '</div>';
+		strHTML += `<p>${arr[counter][3]}</p>
+			</div>
+			</div>`;
+
 		$(".section-content").append(strHTML);
 	}
 }
 
 $(document).ready(function () {
-	getUsers();
+	let users = [
+		"ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck",
+		"habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"
+	  ];
+
+	users.forEach(user => getTwitchUsers(user));
 	displayList(usersAll);
 });
