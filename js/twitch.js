b015a12c19ca27fbe808a13bb9a0b59a6fab3a2f
Leandro <lileandermail+gh+@gmail.com>
2019-06-02 22:40:27 +0800

    Updated

--- js/twitch.js
+++ js/twitch.js
@@ -1,25 +1,5 @@
 // Pseudocode
 // - After loading the page...
- 
-// Check each username in users[] if it exists or not in Twitch.tv via usersAPI
-// - If username exists, push to usersFound[]
-// - If username does not exist, push to usersNotFound[]
-
-// Check each username in usersFound[] if it is streaming or not via streamsAPI
-// - If username is streaming, push to usersStreaming[]
-// - If username is not streaming, push to usersNotStreaming[]
-
-// For each username in usersStreaming[], get its logo, game, status via streamsAPI
-// then push to usersOnline[] and usersAll[]
-
-// For each username in usersNotStreaming[], get its logo, game via channelsAPI and
-// set the status to 'Offline' then push to usersOffline[] and usersAll[]
-
-// Display ALL / ONLINE / OFFLINE users
-// Display ALL users found in usersAll[]
-// Display ONLINE users found in usersOnline[]
-// Display OFFLINE users found in usersOffline[]
-
 
 const BASE_API = 'https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api';
 const USERS_API = BASE_API + "/users/";
@@ -34,30 +14,23 @@ let usersAll = [],
 	strStatus = '', 
 	strHTML = '';
 
-
 async function getTwitchUsers(strUser) {
 	let streamInfo = await fetch(STREAMS_API + strUser);
-
 	if (streamInfo.ok && streamInfo.status === 200) {
 		let streamData = await streamInfo.json();
-	
 		if (streamData.stream !== null) {
 			strLogo = streamData.stream.channel.logo;
 			strGame = streamData.stream.channel.game;
 			strStatus = streamData.stream.channel.status;
-		
 			usersOnline.push([strLogo, strUser, strGame, strStatus]);
-	
 		} else {
 			let channelsInfo = await fetch(CHANNELS_API + strUser);
 			let ChannelsData = await channelsInfo.json();
 			strLogo = ChannelsData.logo;
 			strGame = ChannelsData.game;
 			strStatus = 'offline';
-		
 			usersOffline.push([strLogo, strUser, strGame, strStatus]);
 		}
-	
 		usersAll.push([strLogo, strUser, strGame, strStatus]);
 	}
 }
@@ -91,7 +64,6 @@ $(document).ready(function () {
 		"ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck",
 		"habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"
 	  ];
-
 	users.forEach(user => getTwitchUsers(user));
 	displayList(usersAll);
 });
